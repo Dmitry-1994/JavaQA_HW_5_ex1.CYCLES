d59bbfb982af1ff92d897e3908f7e816a826ee52
Dmitry Tarasov <vizalyyy@gmail.com>
2024-09-30 20:47:05 +0500

    With ParameterizedTest

--- src/test/java/ru/netology/services/WeekendServiceTest.java
+++ src/test/java/ru/netology/services/WeekendServiceTest.java
@@ -1,14 +1,16 @@
 package ru.netology.services;
 
 import org.junit.jupiter.api.Assertions;
-import org.junit.jupiter.api.Test;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.CsvFileSource;
+
 
 public class WeekendServiceTest {
-    @Test
-    public void cntWeekOfMonth() {
+    @ParameterizedTest
+    @CsvFileSource(files = "src/test/resources/weekend.csv")
+    public void cntWeekOfMonth(int income, int expenses, int threshold, int expected) {
         WeekendService service = new WeekendService();
-        int expected = 3;
-        int actual = service.calcCountMonthOff(10_000, 3_000, 20_000);
+        int actual = service.calcCountMonthOff(income, expenses, threshold);
         Assertions.assertEquals(expected, actual);
     }
 }
